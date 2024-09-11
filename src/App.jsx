6657b53ac7a060e7d493098f4dc6a780e615a412
Rimsha30laraib <rimshalarib33@gmail.com>
2024-09-11 23:45:45 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Quiz from "./MyComponents/Quiz.jsx";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Quiz/>
+    </div>
+  )
+}
+
+export default App
